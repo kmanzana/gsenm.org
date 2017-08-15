ba8ac08f0974ab4f90d751e35e52224b82bf126c
Dmanzy21 <icebluedill11@gmail.com>
2017-08-15 09:57:44 -0400

    Initial commit

---
+++ config.rb
@@ -0,0 +1,59 @@
+# Activate and configure extensions
+# https://middlemanapp.com/advanced/configuration/#configuring-extensions
+
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
+
+# Layouts
+# https://middlemanapp.com/basics/layouts/
+
+# Per-page layout changes
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page '/path/to/file.html', layout: 'other_layout'
+
+# Proxy pages
+# https://middlemanapp.com/advanced/dynamic-pages/
+
+# proxy(
+#   '/this-page-has-no-template.html',
+#   '/template-file.html',
+#   locals: {
+#     which_fake_page: 'Rendering a fake page with a local variable'
+#   },
+# )
+
+# Helpers
+# Methods defined in the helpers block are available in templates
+# https://middlemanapp.com/basics/helper-methods/
+
+# helpers do
+#   def some_helper
+#     'Helping'
+#   end
+# end
+
+# Build-specific configuration
+# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
+
+# configure :build do
+#   activate :minify_css
+#   activate :minify_javascript
+# end
+
+require 'dotenv/load'
+
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = 'gsenmpartners.com' # The name of the S3 bucket you are targeting. This is globally unique.
+  s3_sync.region                     = 'us-west-2'     # The AWS region for your bucket.
+  s3_sync.aws_access_key_id          = ENV['ACCESS_KEY']
+  s3_sync.aws_secret_access_key      = ENV['SECRET_KEY']
+  s3_sync.delete                     = false # We delete stray files by default.
+  s3_sync.after_build                = false # We do not chain after the build step by default.
+  s3_sync.prefer_gzip                = true
+end
+
